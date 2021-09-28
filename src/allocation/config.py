7e79ac4f2cc8df55d1bf699568d412b32ef36a22
JWNC9857 <kamal.selvam@orange.com>
2021-09-28 03:36:05 +0200

    Unit of work

--- src/allocation/config.py
+++ src/allocation/config.py
@@ -11,6 +11,6 @@ def get_postgres_uri():
 
 def get_api_url():
     host = os.environ.get('API_HOST', 'localhost')
-    port = 5005 if host == 'localhost' else 80
+    port = 5005 if host == 'localhost' else 5005
     return f"http://{host}:{port}"
 
