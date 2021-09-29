be6722a7130e9ec133f6c533ee89dc36505732aa
JWNC9857 <kamal.selvam@orange.com>
2021-09-29 19:48:36 +0200

    Event handling

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,8 +1,34 @@
 from dataclasses import dataclass
+from typing import Optional
+from datetime import date
 
 
 class Event:
     pass
+
+
+
+
+
+@dataclass
+class BatchCreated(Event):
+    ref: str
+    sku: str
+    qty: int
+    eta: Optional[date] = None
+
+
+@dataclass
+class BatchQuantityChanged(Event):
+    ref: str
+    qty: int
+
+
+@dataclass
+class AllocationRequired(Event):
+    orderid: str
+    sku: str
+    qty: int
 
 
 @dataclass
